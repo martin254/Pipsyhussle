78a72bba866af48f42a2f0b27e216bc8d05dad4f
MartinMuru <mutwiri333@gmail.com>
2026-02-02 00:33:29 +0000

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -24,6 +24,7 @@ import chromecast from "./chrome-cast.svg";
 import sliders04 from "./sliders-04.svg";
 import loading from "./loading.png";
 import background from "./background.jpg";
+import fundingpipz from "./fundingpipz.png";
 
 import curve from "./hero/curve.png";
 import robot from "./hero/robot.jpg";
@@ -156,4 +157,5 @@ export {
   instagram,
   telegram,
   twitter,
+  fundingpipz,
 };
